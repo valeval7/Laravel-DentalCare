46e4d6faad3c1687af6079a5e143d19c19461aee
valeval7 <valeria._mg@hotmail.com>
2025-11-05 23:30:16 -0600

    listo

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   return view('principal.index');
+})->name('inicio');
+
+Route::get('/redirigir', function () {
+  return redirect()->route('inicio');
 });
 
-// Route::get('/redirigir', function () {
-//   return redirect()->route('inicio');
-// });
+Route::get('/sonrisa', function () {
+  return view('principal.sonrisa');
+})->name('sonrisa');
 
-// Route::get('/sonrisa', function () {
-//   return view('principal.sonrisa');
-// })->name('sonrisa');
-
-// Route::get('/agendar', function () {
-//   return view('principal.agendar');
-// })->name('agendar');
+Route::get('/agendar', function () {
+  return view('principal.agendar');
+})->name('agendar');
