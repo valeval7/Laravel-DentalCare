d23b348570ef6c86837d46a3664382c7492ea1d9
valeval7 <valeria._mg@hotmail.com>
2025-11-06 15:12:51 -0600

    Funcional

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Mi Aplicación - @yield('title')</title>
-  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4">
+  </script>
+  <link rel="stylesheet" href="style.css" />
+  @stack('styles')
 </head>
 
 <body class="bg-[#EAEDF3]">
