6b271a2c378d9b6fefb84817a0a7e4bff638bea4
valeval7 <valeria._mg@hotmail.com>
2025-11-05 23:27:26 -0600

    2

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-  return 'Laravel está funcionando sin vistas ni BD';
-});
-
-
-Route::get('/', function () {
   return view('principal.index');
 });
 
-Route::get('/redirigir', function () {
-  return redirect()->route('inicio');
-});
+// Route::get('/redirigir', function () {
+//   return redirect()->route('inicio');
+// });
 
-Route::get('/sonrisa', function () {
-  return view('principal.sonrisa');
-})->name('sonrisa');
+// Route::get('/sonrisa', function () {
+//   return view('principal.sonrisa');
+// })->name('sonrisa');
 
-Route::get('/agendar', function () {
-  return view('principal.agendar');
-})->name('agendar');
+// Route::get('/agendar', function () {
+//   return view('principal.agendar');
+// })->name('agendar');
