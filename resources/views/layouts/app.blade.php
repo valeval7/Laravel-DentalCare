550604f362dbd2955630f395dbcbde20e8194fbb
valeval7 <valeria._mg@hotmail.com>
2025-11-05 23:33:24 -0600

    2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Mi Aplicación - @yield('title')</title>
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-  <link rel="stylesheet" href="style.css" />
-  @stack('styles')
 </head>
 
 <body class="bg-[#EAEDF3]">
